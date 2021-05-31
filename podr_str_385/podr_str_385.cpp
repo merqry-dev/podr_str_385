d1038beca45c01740c205f28ce7efa1703d860eb
merqry <urbanski.janusz@outlook.com>
2021-05-31 23:14:05 +0200

    Second task finished

--- podr_str_385/podr_str_385.cpp
+++ podr_str_385/podr_str_385.cpp
@@ -7,6 +7,11 @@ float calculateFunction(float x)
     return 3 * x * x + x - 2;
 }
 
+float calculateFunction2(float x)
+{
+    return x * x * x + x * x - 2 * x;
+}
+
 bool zeroN(float a, float b, int n, int j, float& x)
 {
     j++;
@@ -25,13 +30,28 @@ bool zeroN(float a, float b, int n, int j, float& x)
         {
             return false;
         }
-
     }
     return true;
 }
 
 bool zeroDelta1(float a, float b, float delta, float& x)
 {
+    x = (a + b) / 2;
+    if (calculateFunction2(x) != 0 && fabs(calculateFunction2(x)) > delta)
+    {
+        if (signbit(calculateFunction2(a)) != signbit(calculateFunction2(x)))
+        {
+            zeroDelta1(a, x, delta, x);
+        }
+        else if (signbit(calculateFunction2(x)) != signbit(calculateFunction2(b)))
+        {
+            zeroDelta1(x, b, delta, x);
+        }
+        else
+        {
+            return false;
+        }
+    }
     return true;
 }
 
@@ -56,5 +76,19 @@ int main()
     {
         cout << "nie ma" << endl;
     }
+
+    //c
+    a = -1;
+    b = 0.5;
+    float delta = 0.01;
+    if (zeroDelta1(a, b, delta, x))
+    {
+        cout << "jest " << x << endl;
+    }
+    else
+    {
+        cout << "nie ma" << endl;
+    }
+
     return 0;
 }
